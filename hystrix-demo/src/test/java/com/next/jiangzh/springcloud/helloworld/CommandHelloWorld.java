7d38820334489afc056d0a0138ea55b70e2fa079
jiangzh <jiangzh@sina.com>
2019-07-30 08:40:07 +0800

    HystrixCommand四种调用方式演示

---
+++ hystrix-demo/src/test/java/com/next/jiangzh/springcloud/helloworld/CommandHelloWorld.java
@@ -0,0 +1,32 @@
+package com.next.jiangzh.springcloud.helloworld;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public class CommandHelloWorld extends HystrixCommand<String> {
+
+    private String name;
+
+    public CommandHelloWorld(String name){
+        // Hystrix必须写groupKey
+        // 服务分组
+        // 如果没有指定线程池，则会用groupKey标识一个线程池
+        super(Setter.withGroupKey(
+                HystrixCommandGroupKey
+                        .Factory.asKey("commandHelloWorld")));
+        this.name = name;
+    }
+
+    /*
+        业务实现方法
+        影院服务 -> 订单服务 -> 查询已售座位信息
+     */
+    @Override
+    protected String run() throws Exception {
+        // 查询已售座位信息
+
+        Thread.sleep(800l);
+
+        return "Run method, name:"+name;
+    }
+}
---
+++ hystrix-demo/src/test/java/com/next/jiangzh/springcloud/helloworld/CommandTest.java
@@ -0,0 +1,113 @@
+package com.next.jiangzh.springcloud.helloworld;
+
+import org.junit.Test;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.functions.Action1;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class CommandTest {
+
+    /*
+        Command同步调用
+     */
+    @Test
+    public void execTest(){
+        long startTime = System.currentTimeMillis();
+        CommandHelloWorld helloWorld = new CommandHelloWorld("Next学院万岁");
+        String result = helloWorld.execute();
+        System.out.println("result="+result+" , spending:"+ (System.currentTimeMillis() - startTime));
+    }
+
+    /*
+       Command异步调用
+    */
+    @Test
+    public void queueTest() throws Exception {
+        long startTime = System.currentTimeMillis();
+        CommandHelloWorld helloWorld = new CommandHelloWorld("Next学院万岁");
+        Future<String> future = helloWorld.queue();
+
+        System.out.println("spending-01:"+ (System.currentTimeMillis() - startTime));
+
+        String result = future.get();
+        System.out.println("result="+result+" , spending-02:"+ (System.currentTimeMillis() - startTime));
+    }
+
+    /*
+        反应式， 触发式
+        observe: Hot调用
+            1、发现subscribe之后，直接先调用run方法
+            2、加载 or 注册 subscribe方法
+            3、调用onNext
+
+        toObservable: Cold调用
+            1、加载 or 注册 subscribe方法
+            2、调用run方法
+            3、调用onNext
+
+     */
+    @Test
+    public void observeTest()throws Exception {
+        CommandHelloWorld helloWorld = new CommandHelloWorld("Next学院万岁");
+        Observable<String> observe = helloWorld.observe();
+        // 阻塞式使用
+        String result = observe.toBlocking().single();
+        System.out.println("result="+result);
+        // 非阻塞式  run -> onNext
+        observe.subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                System.err.println("next: this is onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.err.println("next: s="+throwable.getMessage());
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.err.println("next: s="+s);
+            }
+        });
+    }
+
+    /*
+        反应式， 触发式
+     */
+    @Test
+    public void toObservableTest()throws Exception {
+        CommandHelloWorld helloWorld = new CommandHelloWorld("Next学院万岁");
+        Observable<String> toObservable = helloWorld.toObservable();
+        // 阻塞式使用
+        String result = toObservable.toBlocking().single();
+        System.out.println("result="+result);
+        // 非阻塞式
+        CommandHelloWorld helloWorld2 = new CommandHelloWorld("Next学院万岁");
+        Observable<String> toObservable2 = helloWorld2.toObservable();
+        toObservable2.subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                System.err.println("next: this is onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.err.println("next: s="+throwable.getMessage());
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.err.println("next: s="+s);
+            }
+        });
+        // 让主线程休息两秒
+        Thread.sleep(2000L);
+    }
+
+}
