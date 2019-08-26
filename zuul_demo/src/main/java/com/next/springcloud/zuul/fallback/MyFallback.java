bd1c8762a96d055879a761fdb4866af5c24b4a12
jiangzh <jiangzh@sina.com>
2019-08-27 06:37:40 +0800

    Zuul整合Hystrix完成降级处理

---
+++ zuul_demo/src/main/java/com/next/springcloud/zuul/fallback/MyFallback.java
@@ -0,0 +1,60 @@
+package com.next.springcloud.zuul.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class MyFallback implements FallbackProvider {
+    // 判断哪一个路由要使用该回退降级的方法
+    @Override
+    public String getRoute() {
+        return "hello-service-consumer";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.BAD_REQUEST;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.BAD_REQUEST.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.BAD_REQUEST.name();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream(
+                        "This is fallback method".getBytes("utf-8")
+                );
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
