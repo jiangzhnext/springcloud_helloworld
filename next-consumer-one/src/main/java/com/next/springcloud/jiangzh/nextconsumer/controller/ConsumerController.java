781bb4155353ca9f33719248acdefc66a1212385
jiangzh <jiangzh@sina.com>
2019-08-27 07:15:48 +0800

    Zuul头信息和Cookie处理演示

--- next-consumer-one/src/main/java/com/next/springcloud/jiangzh/nextconsumer/controller/ConsumerController.java
+++ next-consumer-one/src/main/java/com/next/springcloud/jiangzh/nextconsumer/controller/ConsumerController.java
@@ -5,7 +5,11 @@ import com.next.springcloud.jiangzh.nextconsumer.service.ConsumerHelloServiceAPI
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @RestController
 public class ConsumerController {
@@ -17,8 +21,14 @@ public class ConsumerController {
     private ConsumerFeignServiceAPI feignServiceAPI;
 
     @RequestMapping(value = "/consumer/sayHello",method = RequestMethod.GET)
-    public String sayHello(String message){
+    public String sayHello(HttpServletRequest request,@RequestParam("message") String message){
         System.err.println("now Consumer feignServiceAPI Controller-message : "+message);
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            String headName = headerNames.nextElement();
+            System.out.println(headName+" : "+request.getHeader(headName));
+        }
 
         return feignServiceAPI.showHello(message);
     }
